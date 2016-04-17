15a3dd92d8746693b740a788fc9d570d800cf90e
unknown <100485988@UOSL15-R9026733.oncampus.local>
2016-04-16 20:41:24 -0400

    compare full implementations of the greedy solution to the knapsack problem

--- knapsack_problem.hs
+++ knapsack_problem.hs
@@ -3,11 +3,12 @@ import Data.List;
 
 backpack_solve :: [Float] -> [Float] -> [Float] -> Float -> [Float]
 backpack_solve weights values percents capacity
-    | otherwise = backpack_solve(weights values (take (best_item) percents)++[cap_frac]++(drop (best_item)+1 percents) capacity-(cap_frac*weight!!best_item))
+    | capacity<0.0001 = percents
+    | otherwise = backpack_solve weights ((take best_item values)++[0]++(drop (best_item+1) values)) ((take (best_item) percents)++[cap_frac]++(drop (best_item+1) percents)) (capacity-(cap_frac*(weights!!best_item)))
     where
         unit_vals = map (\(v,w) -> v/w) (zip values weights)
         best_item = maxInd unit_vals
-        cap_frac = min 1 (capacity/(weight!!best_item))
+        cap_frac = min 1 (capacity/(weights!!best_item))
 
 
-maxInd list = Data.Maybe.fromJust(Data.List.elemIndex (maximum list))
+maxInd list = Data.Maybe.fromJust(Data.List.elemIndex (maximum list) list)
